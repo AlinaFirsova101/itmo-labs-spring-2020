6aadbf86d1a804b351e83cefc923e00de7c1efda
Alina Firsova <alina_firsova@epam.com>
2020-04-29 02:54:45 +0300

    Error fix_4

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const PORT = 5000
-const func001 = require("./public/FUNC-001/func-001.js");
+// const func001 = require("./public/FUNC-001/func-001.js");
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/FUNC-001', (req, res) => res.render('simple-lab', {code: func001, result: func001()}))
-app.listen(PORT)
+// app.set('views', path.join(__dirname, 'views'))
+// app.set('view engine', 'ejs')
+// app.get('/FUNC-001', (req, res) => res.render('simple-lab', {code: func001, result: func001()}))
+// app.listen(PORT)
