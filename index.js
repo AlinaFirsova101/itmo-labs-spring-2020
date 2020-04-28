fa5f94aef5a2067d2768058d36213924c1474a76
Alina Firsova <alina_firsova@epam.com>
2020-04-29 02:48:33 +0300

    Error fix_3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 5000
 const func001 = require("./public/FUNC-001/func-001.js");
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.get('/', (req, res) => res.sendFile())
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/FUNC-001', (req, res) => res.render('simple-lab', {code: func001, result: func001()}))
