8d6f971b013625f12edca9adcbb012bbaad83d14
Alina Firsova <alina_firsova@epam.com>
2020-04-30 04:31:54 +0300

    Fixed app

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 5000
-const func001 = require("./public/FUNC-001/func-001.js");
+let answer
  
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.get('/FUNC-001', (req, res) => res.render('simple-lab', {code: func001, result: func001()}))
+answer = require("./public/FUNC-002/func-002.js");
+app.get('/FUNC-002', (req, res) => res.render('simple-lab', {code: answer.code, result: answer.result }))
+answer = require("./public/FUNC-003/func-003.js");
+app.get('/FUNC-003', (req, res) => res.render('simple-lab', {code: answer.code, result: answer.result }))
 app.listen(PORT)
