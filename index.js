3f49a6b30ef63f755d546f63c30ec8d8b019a4fe
Alina Firsova <alina_firsova@epam.com>
2020-04-30 04:34:47 +0300

    Error fix_10

--- index.js
+++ index.js
@@ -2,14 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 5000
-let answer
  
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-answer = require("./public/FUNC-002/func-002.js");
-app.get('/FUNC-002', (req, res) => res.render('simple-lab', {code: answer.code, result: answer.result }))
-answer = require("./public/FUNC-003/func-003.js");
-app.get('/FUNC-003', (req, res) => res.render('simple-lab', {code: answer.code, result: answer.result }))
 app.listen(PORT)
