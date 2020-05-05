a67a683dc8c0ad94c0b72f637f183e89e02088bb
Alina Firsova <alina_firsova@epam.com>
2020-05-05 16:20:24 +0300

    Error fix_13

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 5000
  
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
-app.get('/frmw-003', (req, res) => res.sendFile(path.join(__dirname, 'public/frmw-003/dist/index.html')))
+app.get('/frmw-003/', (req, res) => res.sendFile(path.join(__dirname, 'public/frmw-003/dist/index.html')))
 app.listen(PORT)
