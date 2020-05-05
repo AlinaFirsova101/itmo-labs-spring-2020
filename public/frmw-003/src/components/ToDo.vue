499536e09ddbb3472dbc07b435d41d496fe254d4
Alina Firsova <alina_firsova@epam.com>
2020-05-05 16:04:24 +0300

    frmw-003

---
+++ public/frmw-003/src/components/ToDo.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <ul>
+					<ToDoItem
+						v-for="(item, index) of list" 
+						v-bind:key="item"
+						v-bind:item="item"
+						v-bind:index="index"
+						v-on:delete-item="deleteItem"
+					/>
+					<div class="navigation">
+						<input v-model="value"/>
+						<button id="add-btn" @click="addItem(value)"> Add </button>
+					</div>
+				</ul>
+    </div>
+</template>
+
+<script>
+import ToDoItem from '@/components/ToDoItem'
+
+export default {
+    components: {
+        ToDoItem
+		},
+		data() {
+      return { value: '', list: ['one', 'two'] }
+		},
+		methods:{
+			deleteItem(index){
+				this.list.splice(index, 1);
+			},
+			addItem(item){
+				if (this.list.includes(item)){
+					alert('You already have this task!');
+				}
+				else {
+				this.list.push(item);
+				}
+				this.value = '';
+			},
+		}
+}
+</script>
+
+<style scoped> 
+ul {
+	list-style: none;
+	margin: 0;
+	padding: 0;
+
+}
+
+#add-btn {
+	border: 0px;
+	background: rgba(21, 185, 57, 0.938);
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	color: rgb(255, 255, 255);
+	font-size: 25px;
+	margin: 15px;
+}
+
+input {
+	width: 300px;
+	height: 20px;
+	font-size: 13px;
+	border: 1px solid #ccc;
+	background: #F6F6f6;
+	margin: 15px;
+	padding: 10px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	font-size: 20px;
+}
+
+.navigation{
+	display: flex;
+	justify-content: center;
+}
+</style>
---
+++ public/frmw-003/src/components/ToDoItem.vue
@@ -0,0 +1,35 @@
+<template>
+    <li>
+        <span> 
+             <input type="checkbox" v-on:click="$emit('delete-item', index)"/> 
+            {{ item  }} 
+        </span>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        item: {
+            type: String,
+            required: true,
+				},
+				index: {
+            type: Number,
+            required: true,
+        }
+		}
+}
+</script>
+
+<style scoped> 
+	li {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		font-size: 20px;
+		border: 1px solid #ccc;
+		display: flex;
+		justify-content: space-between;
+		padding: .5em 2rem;
+		margin-bottom: 1rem;
+	}
+</style>
