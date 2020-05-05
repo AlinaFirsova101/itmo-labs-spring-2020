4a5c59a83d79b9170659e3a1afb9780bedd59115
Alina Firsova <alina_firsova@epam.com>
2020-05-05 16:24:54 +0300

    Error fix_14

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 5000
+const router = express.Router();
  
 app.use(express.static(path.join(__dirname, 'public')))
-// app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
-app.get('/frmw-003/', (req, res) => res.sendFile(path.join(__dirname, 'public/frmw-003/dist/index.html')))
+app.use('/', router);
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
+router.get('/frmw-003/', (req, res) => res.sendFile(path.join(__dirname, 'public/frmw-003/dist/index.html')))
 app.listen(PORT)
